edd45d36aa2c13ee233f6da5ad5e6ea51045ec5e
Sriram Nambakam <snambakam@microsoft.com>
2023-05-17 22:58:16 +0000

    Add Signal Handler code

---
+++ code/day-1/client-server-over-udp/reserve/mt-server-with-signal/main.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for
+ * details.
+ */
+
+#include "includes.h"
+
+int
+main(int argc, char* argv[]) {
+    uint32_t err = 0;
+    /*
+     * Block selected signals
+     */
+    err = CsUdsServerBlockSignals();
+    BAIL_ON_CSUDS_ERROR(err);
+    /*
+     * Handle Service logic in separate thread which is blocked from selected
+     * signals
+     */
+    err = CsUdsServerStart();
+    BAIL_ON_CSUDS_ERROR(err);
+    /*
+     * Handle signals in main thread
+     */
+    err = CsUdsServerHandleSignals();
+    BAIL_ON_CSUDS_ERROR(err);
+error:
+    return err;
+}
---
+++ code/day-1/client-server-over-udp/reserve/mt-server-with-signal/signal.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for
+ * details.
+ */
+
+#include "includes.h"
+
+static
+void
+CsUdsServerSignalHandler(
+    int signalId
+);
+
+uint32_t
+CsUdsServerBlockSignals(
+    void
+) {
+    uint32_t err = 0;
+    sigset_t originalMask;
+    sigset_t blockedMask;
+
+    sigemptyset(&blockedMask);
+    sigaddset(&blockedMask, SIGINT);
+    sigaddset(&blockedMask, SIGTERM);
+    sigaddset(&blockedMask, SIGHUP);
+    sigaddset(&blockedMask, SIGQUIT);
+    sigaddset(&blockedMask, SIGPIPE);
+
+    err = pthread_sigmask(SIG_BLOCK, &blockedMask, &originalMask);
+    BAIL_ON_CSUDS_ERROR(err);
+error:
+    return err;
+}
+
+uint32_t
+CsUdsServerHandleSignals(
+    void
+) {
+    uint32_t err = 0;
+    struct sigaction action;
+    sigset_t mask;
+    int signalId;
+
+    memset(&action, 0, sizeof(action));
+    action.sa_handler = CsUdsServerSignalHandler;
+
+    sigaction(SIGINT, &action, NULL);
+
+    err = pthread_sigmask(SIG_UNBLOCK, &mask, NULL);
+    BAIL_ON_CSUDS_ERROR(err);
+
+    sigemptyset(&mask);
+    sigaddset(&mask, SIGTERM);
+    sigaddset(&mask, SIGQUIT);
+    sigaddset(&mask, SIGHUP);
+    sigaddset(&mask, SIGPIPE);
+
+    do {
+        sigwait(&mask, &signalId); /* wait for a signal */
+        switch(signalId) {
+            case SIGINT:
+            case SIGQUIT:
+            case SIGTERM:
+            {
+                goto error;
+            }
+            default:
+                break;
+        }
+    } while (1);
+error:
+    return err;
+}
+
+static
+void
+CsUdsServerSignalHandler(
+    int signalId
+) {
+    switch (signalId) {
+        case SIGINT:
+            raise(SIGTERM);
+        default:
+            break;
+    }
+}
